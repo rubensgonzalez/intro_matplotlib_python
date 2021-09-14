e367c87225513383e17e8a49b586854025e24095
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-09-13 22:34:31 -0300

    Update ejercicio_2.py

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -11,6 +11,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot(x,y,y2):
+  
+   fig = plt.figure()
+   fig.suptitle('Gráfico 1',c='g', fontsize=20)
+   ax = fig.add_subplot()    
+   ax.plot(x,y1,c='darkblue', label='Eje "X"')
+   ax.plot(x,y2,c='darkblue', label='Eje "X"')
+   ax.legend()
+   ax.grid()
+   plt.show()
+   
+   
+   
+   
+
+   print("Fin Line Plot")
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
@@ -37,13 +53,21 @@ if __name__ == '__main__':
 
     # Realizar un gráfico que representen las dos funciones
     # Para ello se debe llamar dos veces a "plot" con el mismo "ax"
+    fig = plt.figure()
+    
+    ax = fig.add_subplot()
 
     # Se debe colocar en la leyenda la función que representa
     # cada función
-
     # Cada función dibujarla con un color distinto
     # a su elección
-
+    
+    ax.plot(x, y1, c="r", label="y1 = x**2")
+    ax.plot(x, y2, c="b", label="y2 = x**3")
+     
+    ax.legend()
+   
     # Crear acá su gráfico
 
+    plt.show()
     print("terminamos")
